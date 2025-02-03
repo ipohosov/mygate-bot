f7f43339bf5c602fe5a086709628e12452b3225c
ivan.pohosov <pogosovivan@gmail.com>
2025-02-04 01:19:15 +0200

    update

--- stats.sh
+++ stats.sh
@@ -3,7 +3,7 @@
 DIR="./data/accounts"
 
 # Print the header with nice formatting
-echo -e "Email\t\t\tPoints"
+echo -e "Email\t\t\t     Points   Timer"
 echo -e "-----------------------------------------------------"
 
 # Collect data to create a structured format for column
@@ -13,8 +13,9 @@ for file in "$DIR"/*; do
     if [[ -f $file ]]; then
         email=$(awk -F= '/^Email=/{print $2}' "$file")
         earnings=$(awk -F= '/^Points=/{print $2}' "$file")
+        timer=$(awk -F= '/^Timer=/{print $2}' "$file")
         if [[ -n $email && -n $earnings ]]; then
-            output+="$email\t$earnings\n"
+            output+="$email\t$earnings\t\t$timer\n"
         fi
     fi
 done
