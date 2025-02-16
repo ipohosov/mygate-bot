12900f0b17a84ef78d54f5d29ec2c18585070825
ivan.pohosov <pogosovivan@gmail.com>
2025-02-16 16:20:50 +0200

    Add today points

--- stats.sh
+++ stats.sh
@@ -4,7 +4,7 @@ DIR="./data/accounts"
 
 # Print the header with nice formatting
 echo -e "Email\t\t\t     Points\tToday Points\tTimer"
-echo -e "-----------------------------------------------------"
+echo -e "---------------------------------------------------------------------"
 
 # Collect data to create a structured format for column
 output=""
@@ -16,18 +16,18 @@ for file in "$DIR"/*; do
         today_earnings=$(awk -F= '/^Today_Points=/{print $2}' "$file")
         timer=$(awk -F= '/^Timer=/{print $2}' "$file")
         if [[ -n $email && -n $earnings ]]; then
-            output+="$email\t$earnings\t$today_earnings\t\t$timer\n"
+            output+="$email\t$earnings\t\t\t$today_earnings\t\t\t$timer\n"
         fi
     fi
 done
 
 # Use column to format the output and sort by Total_Earnings
 echo -e "$output" | sort -k2,2n | column -t -s $'\t'
-echo -e "-----------------------------------------------------"
+echo -e "---------------------------------------------------------------------"
 filename='./data/accounts/stopped_accounts.txt'
 if [ -e "$filename" ]; then
   echo -e "Stopped accounts:"
   cat filename
-  echo -e "-----------------------------------------------------"
+  echo -e "---------------------------------------------------------------------"
 fi
 
